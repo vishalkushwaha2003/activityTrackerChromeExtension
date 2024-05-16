299e01f68a7a03e8739b81e8f24b6717fd8579f8
vishalkushwaha2003 <vishal2003kushwaha>
2024-05-16 22:30:43 +0530

    add timer and remove unecessary comments

--- public/background.js
+++ public/background.js
@@ -26,77 +26,7 @@ chrome.windows.onCreated.addListener((window) => {
   // });
 });
 
-// function updateActiveTab(tabId) {
-//   chrome.tabs.get(tabId, function (tab) {
-//     if (tab.active && tab.windowId !== chrome.windows.WINDOW_ID_NONE) {
-//       if (!activeTabs[tabId]) {
-//         activeTabs[tabId] = {
-//           startTime: new Date().getTime(),
 
-//           url: tab.url,
-//           totalTime: 0,
-//         };
-//         if (preTabId == null) {
-//           preTabId = tabId;
-//         } else {
-//           const preFocusDuration =
-//             new Date().getTime() - activeTabs[preTabId].startTime;
-
-//           activeTabs[preTabId].totalTime =
-//             activeTabs[preTabId].totalTime + preFocusDuration;
-
-//           preTabId = tabId;
-//         }
-//       } else {
-//         if (preTabId !== null && tabId !== preTabId) {
-//           const preFocusDuration =
-//             new Date().getTime() - activeTabs[preTabId].startTime;
-
-//           activeTabs[preTabId].totalTime =
-//             activeTabs[preTabId].totalTime + preFocusDuration;
-
-//           preTabId = tabId;
-//           activeTabs[tabId].startTime = new Date().getTime();
-//         }
-//       }
-
-//       // console.log(activeTabs);
-//       // console.log(activeTabs[tabId].totalTime);
-//     }
-//   });
-// }
-
-//   chrome.tabs.onUpdated.addListener((tabId, changeInfo,tab) => {
-//                         console.log(tab)
-
-//   });
-
-// chrome.tabs.onActivated.addListener((activeInfo) => {
-//       chrome.tabs.get(activeInfo.tabId,(tab1)=>{
-
-//         if(tab1.url!="" && tab1.url!="chrome://newtab/"){
-//           chrome.tabs.onUpdated.addListener((p,t,tab2)=>{
-//             if(tab2.status==='complete')console.log(tab2)
-//           }
-
-//           )
-
-//         }
-//       })
-
-//   updateActiveTab(activeInfo.tabId);
-// });
-
-// chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
-//   console.log(tabId);
-//   console.log(removeInfo);
-//   if (activeTabs[tabId]) {
-//     preTabId = tabId;
-//     activeTabs[tabId].startTime = new Date().getTime();
-
-//     // delete activeTabs[tabId];
-//   }
-// });
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   const dataToSend = { hiii: "vishal" };
@@ -104,60 +34,7 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   chrome.runtime.sendMessage({ action: "sendDataToPopup", data: dataToSend });
 });
 
-// const filters = {
-//   url: [{ hostContains: 'example.com' }] // Add any filters you need
-// };
-
-//timer
-
-// let startTime =0;
-// let elapsedTime = 0;
-// let timerInterval;
-// let currentTime=0;
-
-// function updateTime() {
-//    currentTime = Date.now();
-//   elapsedTime = currentTime - startTime;
-//   displayTime(elapsedTime);
-// }
-
-// function startTimer() {
-//   startTime = Date.now() - elapsedTime;
-//   timerInterval = setInterval(updateTime, 1000);
-// }
-
-// function stopTimer() {
-//   clearInterval(timerInterval);
-// }
-
-// function resetTimer() {
-//   clearInterval(timerInterval);
-//   elapsedTime = 0;
-//   displayTime(elapsedTime);
-// }
-
-// function displayTime(time) {
-//   const totalSeconds = Math.floor(time / 1000);
-//   const hours = Math.floor(totalSeconds / 3600);
-//   const minutes = Math.floor((totalSeconds % 3600) / 60);
-//   const seconds = totalSeconds % 60;
-//    console.log(`${hours}:${minutes}:${seconds}`)
-
-// }
-
-// chrome.webNavigation.onCompleted.addListener((t) => {
-//   if (t.url!="chrome://new-tab-page/" && t.frameType === "outermost_frame") {
-//     console.log("webnavigation information", t);
-//   }
-// });
-
-// chrome.tabs.onActivated.addListener((tab)=>{
-
-//    console.log(tab)
-
-// }
-
-// )
+//on acctive tab listener
 chrome.tabs.onActivated.addListener(() => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tab) => {
     if (
@@ -194,11 +71,11 @@ chrome.tabs.onActivated.addListener(() => {
     }
   });
 });
-
+//on update tabs events
 chrome.tabs.onUpdated.addListener((p, t, tab2) => {
   if (tab2.url != "chrome://newtab/" && preTab != tab2.url) {
     if (preTab != "" && activeTabs[preTab]) {
-      // console.log(preTab);
+      
       stopTimer();
       activeTabs[preTab].totalTime = `${hours}:${minutes}:${seconds}`;
       console.log(preTab);
@@ -218,6 +95,7 @@ chrome.tabs.onUpdated.addListener((p, t, tab2) => {
     preTab = tab2.url;
   }
 });
+
 
 //timer
 let startTime = 0;
--- public/timer.js
+++ public/timer.js
@@ -1,3 +1,5 @@
-function helperFunction() {
+ function timer() {
     console.log("teri maa ki chuut")
 }
+
+ex
